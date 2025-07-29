b7a4cccd897c4c1436a1b617a6de6171b6994c8c
hummingbirdtestai <hummingbirdtestai@gmail.com>
2025-07-29 18:59:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import app from './app.js';
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
